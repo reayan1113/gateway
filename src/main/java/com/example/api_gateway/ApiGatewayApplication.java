c1efc20b3d33e0361eac27695d722f890ce07ad0
Nidula Dharmapriya <nidula@example.com>
2026-02-16 18:52:21 +0530

    fix/ debuging endpoints implemented

--- src/main/java/com/example/api_gateway/ApiGatewayApplication.java
+++ src/main/java/com/example/api_gateway/ApiGatewayApplication.java
@@ -10,4 +10,25 @@ public class ApiGatewayApplication {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
+	@org.springframework.context.annotation.Bean
+	public org.springframework.boot.CommandLineRunner logRoutes(
+			org.springframework.cloud.gateway.route.RouteLocator routeLocator) {
+		return args -> {
+			System.out.println(
+					"========================================================================================");
+			System.out.println(
+					"                            LOADED GATEWAY ROUTES                                       ");
+			System.out.println(
+					"========================================================================================");
+			routeLocator.getRoutes().subscribe(route -> {
+				System.out.println("Route ID: " + route.getId());
+				System.out.println("   URI: " + route.getUri());
+				System.out.println("   Predicate: " + route.getPredicate());
+				System.out.println("   Filters: " + route.getFilters());
+				System.out.println(
+						"----------------------------------------------------------------------------------------");
+			});
+		};
+	}
+
 }
